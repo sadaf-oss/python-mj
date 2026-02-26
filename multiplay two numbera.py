c27ad59a481f366941c75b3de05e5ba3178ebc00
Sadaf-oss <s46319497>
2026-02-26 15:57:53 -0800

    base

---
+++ multiplay two numbera.py
@@ -0,0 +1,8 @@
+# write a program that two integers values from the user
+# and multiply to output result.
+
+
+num1 = int(input("enter your choice"))
+num2 = int(input("enter your choice"))
+multiply =  num1 * num2
+print("the result of multiplication is:",multiply)
