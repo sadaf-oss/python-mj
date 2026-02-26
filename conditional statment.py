740996831f3a63a887bb2fe0dea607aa43cc4ff5
Sadaf-oss <s46319497>
2026-02-26 15:49:15 -0800

    second

---
+++ conditional statment.py
@@ -0,0 +1,43 @@
+#conditional statement
+# condition
+# conditional operators
+     # > greater than
+     # < less than
+     # == is equal
+     # != not equal
+     # >= greater than or equal
+     # <= less than or equal
+
+# print(12 > 5)
+# print(14 < 3)
+# print(4 == 4)
+# print(2!= 2)
+
+# print(15 >= 5)
+
+# value1 = 34
+# val = 20
+# print(value1 <= val)
+
+# if single
+# if-else two block
+# if-elif-else multiblock
+
+if 5 > 10:
+    print("50 is greater than 10")
+    print("we are now inside the if block")
+    
+# write a program to show marks and find its percentage 
+# say congratualtion if marks are above 60.
+marks = float(input("enter your marks:"))
+
+if marks > 60:
+    print("congratulations you're passed", marks)
+    percetange = marks/100*100
+    print("your percentage is:",percetange, "%" )
+
+print("program end")    
+
+
+
+
