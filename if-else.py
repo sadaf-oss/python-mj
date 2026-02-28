d2701e10a6b760ac25161db562fd533dbe7178bd
Sadaf-oss <s46319497>
2026-02-28 15:53:26 -0800

    if-eles.py

---
+++ if-else.py
@@ -0,0 +1,22 @@
+# age = float(input("enter your age"))
+
+# if age >= 18:
+#      print("Welcome you're able to proceed you ID card")
+
+
+# marks = float(input("enter your marks"))
+
+# if marks >= 60:
+#      print("Congratulations you're passed")
+# else:
+#      print("Sorry you're failed, try gain")
+
+
+# check wether the number is positive or negative
+
+number = int(input("please emter a number of your choice>>"))
+
+if number >0:
+    print("The number",number,"is Positive")
+else:
+    print("The number",number,"is Negative")
